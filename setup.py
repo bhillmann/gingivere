9295063dda53cded0dbfc48ca26d8f70c5840e89
Ben Hillmann <bhillmann@outlook.com>
2014-10-10 19:28:43 -0500

    Setup the directory skeleton.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='gingivere',
+    version='1.0',
+    packages=['gingivere'],
+    url='',
+    license='MIT',
+    author='Ben Hillmann',
+    author_email='bhillmann@outlook.com',
+    description=''
+)
