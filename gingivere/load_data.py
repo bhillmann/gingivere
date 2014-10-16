fcfe99fc22731886855b5d7254af1d8964c7b772
Ben Hillmann <bhillmann@outlook.com>
2014-10-16 11:50:00 -0500

    Added the new line for the git ignore. Changed line spacings.

--- gingivere/load_data.py
+++ gingivere/load_data.py
@@ -1,13 +1,37 @@
 from config import *
-import theano
 import scipy.io
+# from sklearn.decomposition import PCA
+#
+# from sklearn.feature_selection import SelectKBest
+# from sklearn.feature_selection import chi2
+#
+# import tables
 
-if __name__ == "__main__":	
-	mat = scipy.io.loadmat(DATA_DIR + "Dog_22/Dog_2_preictal_segment_0001.mat")
-	xx = mat['preictal_segment_1'][0,0]
-	data = xx[0]
-	data_length_sec = xx[1]
-	sampling_frequency = xx[2]
-	channels = xx[3]
-	sequence = xx[4]
-	print(DATA_DIR)
+
+def load_mat(dir):
+    # mat = scipy.io.loadmat(DATA_DIR + "Dog_22/Dog_2_preictal_segment_0001.mat")
+    # xx = mat['preictal_segment_1'][0,0]
+    # data = xx[0]
+    # data_length_sec = xx[1]
+    # sampling_frequency = xx[2]
+    # channels = xx[3]
+    # sequence = xx[4]
+    # return{
+
+    # }
+    pass
+
+
+if __name__ == "__main__":
+    mat = scipy.io.loadmat(DATA_DIR + "Dog_22/Dog_2_preictal_segment_0001.mat")
+    xx = mat['preictal_segment_1'][0, 0]
+    data = xx[0]
+    data_length_sec = xx[1]
+    sampling_frequency = xx[2]
+
+    channels = xx[3]
+    # Clean the channels
+    channels = [x[0] for x in channels[0]]
+    sequence = xx[4]
+    print(DATA_DIR)
+
