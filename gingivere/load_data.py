aa2ca4ccacedab8aa3ddc1e80d098f216413b137
Ben Hillmann <bhillmann@outlook.com>
2014-10-16 13:00:09 -0500

    Created the PCA transform

--- gingivere/load_data.py
+++ gingivere/load_data.py
@@ -1,6 +1,6 @@
 import scipy.io
 import json
-# from sklearn.decomposition import PCA
+from sklearn.decomposition import PCA
 #
 # from sklearn.feature_selection import SelectKBest
 # from sklearn.feature_selection import chi2
@@ -8,25 +8,8 @@ import json
 # import tables
 
 
-def load_mat(dir):
-    # mat = scipy.io.loadmat(DATA_DIR + "Dog_22/Dog_2_preictal_segment_0001.mat")
-    # xx = mat['preictal_segment_1'][0,0]
-    # data = xx[0]
-    # data_length_sec = xx[1]
-    # sampling_frequency = xx[2]
-    # channels = xx[3]
-    # sequence = xx[4]
-    # return{
-
-    # }
-    pass
-
-
-if __name__ == "__main__":
-    with open('config.json', 'r') as f:
-        DATA_DIR = json.load(f)
-        f.close()
-    mat = scipy.io.loadmat(DATA_DIR + "Dog_22/Dog_2_preictal_segment_0001.mat")
+def load_mat(path, number=1, state='preictal'):
+    mat = scipy.io.loadmat(path + "Dog_22/Dog_2_%s_segment_%04d.mat" % (state, number))
     xx = mat['preictal_segment_1'][0, 0]
     data = xx[0]
     data_length_sec = xx[1]
@@ -36,4 +19,22 @@ if __name__ == "__main__":
     # Clean the channels
     channels = [x[0] for x in channels[0]]
     sequence = xx[4]
-    print(DATA_DIR)
+    return data
+
+
+def pca_reduce(data):
+    pca = PCA(n_components=2)
+    return pca.fit_transform(data)
+
+
+def main():
+    with open('config.json', 'r') as f:
+        DATA_PATH = json.load(f)
+        f.close()
+    data = load_mat(DATA_PATH)
+    xx = pca_reduce(data)
+    return xx
+
+
+if __name__ == "__main__":
+    main()
