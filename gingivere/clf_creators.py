b2b31b258c601558c6b6c7da8175156f8169dd3a
Benjamin Hillmann <bhillmann@outlook.com>
2014-10-21 22:25:11 -0500

    Added the new main functions.

--- gingivere/clf_creators.py
+++ gingivere/clf_creators.py
@@ -7,6 +7,15 @@ def make_clf():
     estimators = [('reduce_dim', PCA()), ('svm', SVC())]
     return Pipeline(estimators)
 
+
 def make_gs():
     params = dict(reduce_dim__n_components=[2, 5, 10], svm__C=[0.1, 10, 100])
     return GridSearchCV(clf, param_grid=params)
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
