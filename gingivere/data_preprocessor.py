23b63f141cc1da62a81293dc1002d63d0fea5db2
Benjamin Hillmann <bhillmann@outlook.com>
2014-10-22 00:16:36 -0500

    Started to use the shelve module. Won't work fast enough.

--- gingivere/data_preprocessor.py
+++ gingivere/data_preprocessor.py
@@ -1,9 +1,10 @@
-def get_standard_deviations(data):
-    return np.std(data,axis=1)
+import shelve
+import load_data
 
-def main():
-    pass
-
-
-if __name__ == "__main__":
-    main()
+for data in load_data.walk_training_mats("Dog_2"):
+    for i, item in enumerate(data['data']):
+        s = shelve.open('./data/dog_2/dog_2', writeback=True)
+        try:
+            s["%02d_%s" % (i, data['file'])] = item
+        finally:
+            s.close()
