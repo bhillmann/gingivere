c3bb6fa96f6a85ab0ff7d032eb4e895c93236953
Ben Hillmann <bhillmann@outlook.com>
2014-12-08 17:06:40 -0800

    Added the classifiers wrapper.

---
+++ gingivere/classifiers.py
@@ -0,0 +1,17 @@
+import numpy as np
+import sklearn
+import sklearn.pipeline
+from sklearn.linear_model import LogisticRegression, LinearRegression
+from sklearn.preprocessing import StandardScaler
+
+
+class SimpleLogisticRegression(LinearRegression):
+    def predict_proba(self, X):
+        predictions = self.predict(X)
+        predictions = sklearn.preprocessing.scale(predictions)
+        predictions = 1.0 / (1.0 + np.exp(-0.5 * predictions))
+        return np.vstack((1.0 - predictions, predictions)).T
+
+
+def make_simple_lr():
+    return (sklearn.pipeline.make_pipeline(StandardScaler(), SimpleLogisticRegression()), 'ss-slr')
