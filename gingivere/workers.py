9542b6a78bb2224e00304a698476fc4588b7ab7d
Ben Hillmann <bhillmann@outlook.com>
2014-12-09 11:35:56 -0800

    Refactored names in worker.

--- gingivere/workers.py
+++ gingivere/workers.py
@@ -1,12 +1,9 @@
 from multiprocessing import Pool
 
-from gingivere.data import tuple_to_mat, generate_names
+from gingivere.data import generate_paths
 from gingivere import SETTINGS
 
 def generate_target_data(target):
     pool = Pool(SETTINGS.N_jobs)
     results = pool.map(consume_mat, [t for t in generate_names(target)])
     return results
-
-def consume_mat(t):
-    return tuple_to_mat(t)['data']
