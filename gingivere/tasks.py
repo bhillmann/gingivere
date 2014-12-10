8cda9e5c049f0c7cc6f58623c5268d3c0ad2e3d9
Ben Hillmann <bhillmann@outlook.com>
2014-12-09 17:59:47 -0800

    Revert "Migrated workers into task."
    This reverts commit 5dd48b04081ef8b5737f1fa48853a02cfe7007d7.

--- gingivere/tasks.py
+++ gingivere/tasks.py
@@ -18,10 +18,10 @@ def build_data_for_cv(data):
         elif 'preictal' in path:
             y = y + [1] * num_feature_vecs
     y = np.array(y, dtype='float32')
-    return X, y, paths
+    return X, y
 
 def train_classifier(data):
-    X, y, paths = data
+    X, y = data
     clf, name = make_simple_lr()
     skf = StratifiedKFold(y, n_folds=2)
     for train_index, test_index in skf:
@@ -46,16 +46,3 @@ def train_classifier(data):
         print(roc_auc_score(y_true, y_pred))
         print()
     return clf
-
-
-class TransformationPipeline:
-    def __init__(self, transformations):
-        self.transformations = transformations
-
-    def run(self, origin):
-        destination = origin
-        for transform, kargs in self.transformations:
-            destination = transform(destination, **kargs)
-        return destination
-
-# (funct,kargs={})
