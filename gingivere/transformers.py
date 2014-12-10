2fcc100cb5d3dbbde8dc457ad95c85fe00ce0310
Ben Hillmann <bhillmann@outlook.com>
2014-12-09 16:10:47 -0800

    Stopped automatic printing.

--- gingivere/transformers.py
+++ gingivere/transformers.py
@@ -8,11 +8,7 @@ from gingivere.data import load_mat_from_path
 memory = Memory(SETTINGS.cache_dir, mmap_mode='r+')
 
 def source(path):
-    print("Loading: %s" % path)
-    return load_mat_from_path(path)['data'].values
-
-@memory.cache
-def source_mem(path):
+    # print("Loading: %s" % path)
     return load_mat_from_path(path)['data'].values
 
 def preprocess(origin):
