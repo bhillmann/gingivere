50451e7f94550fae83a4923ec4f22ea82fe0c5ec
Ben Hillmann <bhillmann@outlook.com>
2014-12-14 21:17:02 -0600

    Readying up for score accumulation.

--- gingivere/postprocess.py
+++ gingivere/postprocess.py
@@ -1,11 +1,9 @@
 import numpy as np
-from sklearn.metrics import classification_report
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.cross_validation import StratifiedKFold
 
 def postprocess_data(target, X, y, paths, trainers, submission=False):
     XX, yy = [], []
-    for pred, target in accumulate_scores(X, y, trainers, paths):
+    for pred, target in accumulate_scores(paths):
         XX += list(pred)
         yy += list(target)
     scores_for_clf_2(np.array(XX), np.array(yy))
@@ -17,35 +15,15 @@ def generate_mask_for_mats(paths):
     for unique_path in set(paths):
         yield np.array([unique_path == path for path in paths], dtype='bool')
 
+def accumulate_scores(X, y, clf, paths):
+    target = []
+    pred = []
+    for mask in generate_mask_for_mats(paths):
+        pred.append(clf.predict_proba(X[mask])[:, 1])
+        target.append(y[mask][0])
+    yield np.array(pred), np.array(target)
 
-def accumulate_scores(X, y, trainers, paths):
-    for train_index, test_index, clf in trainers:
-        target = []
-        pred = []
-        for mask in generate_mask_for_mats(paths):
-            pred.append(clf.predict_proba(X[mask])[:, 1])
-            target.append(y[mask][0])
-        yield np.array(pred), np.array(target)
-
-
-
-
-def scores_for_clf(X, y, trainers):
-    for train_index, test_index, clf in trainers:
-        print("Detailed classification report:")
-        print()
-        print("The model is trained on a folded development set.")
-        print("The scores are computed on the full set.")
-        print()
-        X_train, X_test = X[train_index], X[test_index]
-        y_train, y_test = y[train_index], y[test_index]
-        y_true, y_pred = y_test, clf.predict_proba(X_test)
-        print(classification_report(np.around(y_true), np.around(y_pred[:, 1])))
-        print()
-        print(roc_auc_score(y_true, y_pred[:, 1]))
-        print()
-
-def scores_for_clf_2(X, y):
+def accumulate_scores_for_clf(X, y):
     clf = RandomForestClassifier(n_estimators=10, max_depth=None, min_samples_split=1, random_state=0)
     skf = StratifiedKFold(y, n_folds=2)
     for train_index, test_index in skf:
@@ -63,11 +41,11 @@ def scores_for_clf_2(X, y):
         print(roc_auc_score(y_true, y_pred[:, 1]))
         print()
 
-def scores_for_clf_3(X, y):
-    y_pred = np.array([np.mean(x) for x in X])
-    print("Detailed classification report:")
-    print()
-    print(classification_report(np.around(y), np.around(y_pred)))
-    print()
-    print(roc_auc_score(y, y_pred))
-    print()
+# def scores_for_clf_3(X, y):
+#     y_pred = np.array([np.mean(x) for x in X])
+#     print("Detailed classification report:")
+#     print()
+#     print(classification_report(np.around(y), np.around(y_pred)))
+#     print()
+#     print(roc_auc_score(y, y_pred))
+#     print()
--- gingivere/process.py
+++ gingivere/process.py
@@ -1,6 +1,8 @@
 from classifiers import make_simple_lr
+import numpy as np
 from sklearn.cross_validation import StratifiedKFold
 from sklearn.metrics import roc_auc_score
+from sklearn.metrics import classification_report
 
 from gingivere.utilities.shelve import insert_shelve
 
@@ -25,7 +27,7 @@ def process_data(target, X, y, paths, submission=False, clf=False):
         clf = save_best_classifier(target, X, y, trainers)
     return X, y, paths, clf
 
-def save_best_classifier(target, X, y, trainers):
+def save_best_classifier(target, X, y, trainers, verbose=True):
     print("Saving the best classifier for: %s" % target)
     best = float('inf'), False
     for train_index, test_index, clf in trainers:
@@ -33,6 +35,16 @@ def save_best_classifier(target, X, y, trainers):
         y_train, y_test = y[train_index], y[test_index]
         y_true, y_pred = y_test, clf.predict_proba(X_test)
         curr_score = roc_auc_score(y_true, y_pred[:, 1])
+        if verbose:
+            print("Detailed classification report:")
+            print()
+            print("The model is trained on a folded development set.")
+            print("The scores are computed on the full set.")
+            print()
+            print(classification_report(np.around(y_true), np.around(y_pred[:, 1])))
+            print()
+            print(curr_score)
+            print()
         if curr_score < best[0]:
             best = curr_score, clf
     insert_shelve(clf, '%s_clf' % target)
